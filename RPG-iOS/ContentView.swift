d2a3595dfef662769b2a420c96cfd665d4472994
kenmaz <kentaro.matsumae@gmail.com>
2025-06-14 21:14:49 +0900

    Initial Commit

---
+++ RPG-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RPG-iOS
+//
+//  Created by kenmaz on 2025/06/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RPG-iOS/RPG_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPG_iOSApp.swift
+//  RPG-iOS
+//
+//  Created by kenmaz on 2025/06/14.
+//
+
+import SwiftUI
+
+@main
+struct RPG_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
